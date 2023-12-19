78cc007857d28836391fd35ea46d1815b439d9af
Abdiaziz2233 <zizoalhusyny73@gmail.com>
2023-12-19 15:04:14 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const ourForm = document.getElementById("create-task-form")
+  ourForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const inputValue = document.getElementById("new-task-description").value
+    createNode(inputValue)
+    ourForm.reset()
+  })
 });
+
+const ulList = document.getElementById("tasks")
+
+function createNode(inputValue){
+    const li = document.createElement("li")
+    const btn = document.createElement("button")
+    li.textContent = `${inputValue} `
+    btn.textContent = " X"
+    li.appendChild(btn)
+    ulList.appendChild(li)
+
+    btn.addEventListener("click", (e) =>{
+        e.target.parentNode.remove()
+    })
+}
